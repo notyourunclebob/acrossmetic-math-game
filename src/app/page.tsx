c1d65f5d96ade4c833921d58d4376831dba5f2b3
notyourunclebob <jw.pcapp@gmail.com>
2026-01-08 19:38:06 -0400

    add dynamic to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Gamegrid from "@/components/Gamegrid";
 import { GameData, GameOptions } from "@/tools/game.model";
 import { defaultGameOptions, generateGame } from "@/tools/GameManager";
